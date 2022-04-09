069e70a61831b24a87e22bfce8f3b7948c0a16dc
Renato Figueiredo <renatofigueiredoalves@gmail.com>
2022-04-09 19:43:12 -0300

    Hooks

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,22 +1,25 @@
+import { useState, useEffect } from 'react';
 import { RepositoryItem } from "./RepositoryItem";
 
-const repository = {
-    name: 'unform',
-    description: 'Form in React',
-    link: 'https://www.google.com.br'
-}
-
+import '../styles/repositories.scss';
 export function RepositoryList() {
-    return(
-			<section className="repository-list">
-			<h1>Lista de repositórios</h1>
+	const [repositories, setRepositories] = useState([]);
 
-			<ul>
-				<RepositoryItem repository={repository}/>
-				<RepositoryItem repository={repository}/>
-				<RepositoryItem repository={repository}/>
-				<RepositoryItem repository={repository}/>
-			</ul>
-			</section>
+	useEffect(() => {
+		fetch('https://api.github.com/orgs/rocketseat/repos')
+		.then(response => response.json())
+		.then(data => setRepositories(data))
+	}, []);
+
+	return(
+		<section className="repository-list">
+		<h1>Lista de repositórios</h1>
+
+		<ul>
+			{repositories.map(repository => {
+				return <RepositoryItem key={repository.name} repository={repository} />
+			})}
+		</ul>
+		</section>
 	)
 }
